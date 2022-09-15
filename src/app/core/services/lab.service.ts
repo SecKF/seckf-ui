7f929466ef1e9b80d14d4bcf339a0cf4ba51b7f6
Pablo Barrientos <pablo.barrientos.13@gmail.com>
2022-09-15 09:05:23 +0200

    Initial code. Tests not working due to node version

---
+++ src/app/core/services/lab.service.ts
@@ -0,0 +1,46 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LabService
+{
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public authHeader = new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': sessionStorage.getItem("Authorization") });
+
+  getLabs(): Observable<Object>
+  {
+    return this.http.get(environment.API_ENDPOINT + '/api/interactive_labs/items')
+  }
+
+  deployLab(image_id: number): Observable<Object>
+  {
+    return this.http.get(environment.API_ENDPOINT + `/api/interactive_labs/deployments/${image_id}`, { headers: this.authHeader })
+  }
+
+  deleteLab(image_id: number): Observable<Object>
+  {
+    return this.http.get(environment.API_ENDPOINT + `/api/interactive_labs/delete-deployments/${image_id}`, { headers: this.authHeader })
+  }
+
+  getCodeLabs(code_type: string): Observable<Object>
+  {
+    return this.http.get(environment.API_ENDPOINT + `/api/code_review_labs/challenge/${code_type}`, { headers: this.authHeader })
+  }
+
+
+  solveCodeReviewChallenge(code_id: number, solution_id: number): Observable<Object>
+  {
+    return this.http.get(environment.API_ENDPOINT + `/api/code_review_labs/solve_challenge/${code_id}/solution/${solution_id}`, { headers: this.authHeader })
+  }
+
+}
